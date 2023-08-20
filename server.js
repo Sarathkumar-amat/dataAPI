d6bac99dfd16edd0db14ebf4064d5cc34ebae2ba
Sarathkumar-amat <poongodinall@gmail.com>
2023-08-20 16:09:16 +0530

    adding server

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const { electricity } = require('./data');
+
+const app = express();
+const port = 3000;
+
+// Middleware to parse JSON data in the request body
+app.use(bodyParser.json());
+
+// Sample JSON data array
+const data = electricity;
+
+// POST endpoint to respond with the sample data array
+app.post('/api/data', (req, res) => {
+  res.json(sampleData);
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
